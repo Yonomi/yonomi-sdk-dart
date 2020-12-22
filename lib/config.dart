2f0c3dcef772ea9802903140316b3f7857eeb992
Sergio E <ses110@gmail.com>
2020-12-22 15:56:13 -0500

    Use safe_config library to parse yaml file and set token and endpoint

--- lib/config.dart
+++ lib/config.dart
@@ -1,6 +1,21 @@
-class CONFIG {
-  static final String TOKEN =
-      'eyJhbGciOiJSUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIyNTIyZDdhMC1hMTQ4LTQ3YjItOThhZC05ZGRjZmY0MjlhMDUiLCJpc3MiOiJjMTM2NDI2Zi1iNDM2LTQ3MmItYjM5Mi0wOGE1YjgyOWY4ZDQiLCJpYXQiOjE2MTAwMjAwNjYsImV4cCI6MTYxMDEwNjQ2Nn0.G7Kxfe01M34Zbu4AZ7O1ULlXKc0-u5UCxE9yh4PqH7HidU0E9pg7DvQR3kwefp8fZyonfiEvrr4I74MU41br-gyGEcswXzTi2QHI2qIvrFDgtb1PDUop-mkNMYqFLARR3l32IpZXMCZt4cz36Hpc3Q2elLk1z4DddmGuWFVgMoAEWhVtTrG17Xh5VFmmM2ZiE5OrEMfGvV-Q3yzq_noi9IIHVaxHaOKKhV6ChAPcWMhQtiFZYnUUj143686PXfh_FI0Hfw54FjdPFiPZGxw8eupON5gkfSEVp8Jzyjwy5DPjHfzB7UV7W3TrolQQHZVO9bpEireeip-knRR6Tb8HzQ';
-  static final String URL =
-      'https://co19ogympg.execute-api.us-east-1.amazonaws.com/stg/graphql';
+import 'dart:io';
+
+import 'package:safe_config/safe_config.dart';
+
+class CONFIG extends Configuration {
+  static final CONFIG _instance = CONFIG._privateConstructor();
+
+  factory CONFIG() {
+    return _instance;
+  }
+
+  CONFIG._privateConstructor() : super.fromFile(File("config.yaml"));
+
+  @optionalConfiguration
+  static final String TOKEN = _instance.authToken;
+
+  @optionalConfiguration
+  static final String URL = _instance.graphEndpoint;
+
+  String graphEndpoint, authToken;
 }
