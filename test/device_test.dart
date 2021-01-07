81859dd8557403dd59927f488e7fb3ecd4df5f54
Sergio E <ses110@gmail.com>
2021-01-07 14:10:05 -0600

    test: make sure new fields get parsed and populated correctly

--- test/device_test.dart
+++ test/device_test.dart
@@ -2,6 +2,8 @@ import 'package:test/test.dart';
 import 'package:yonomi_platform_sdk/device.dart';
 
 void main() {
+  var testDeviceId = "f111c2f8-b928-4e5a-9e67-192196fd444f";
+  
   test('find() should create a device with default query', () {
     Device user = Device.findById("someId");
     expect(
@@ -22,26 +24,72 @@ void main() {
 
   test('get() should return device with default value', () async {
     Device device =
-        await Device.findById("616e8382-bb26-41f5-8861-eeb62525d606").get();
+        await Device.findById(testDeviceId).get();
     expect(device.id, isNotNull);
   });
 
-  test('project() should return projections for firstActivityAt', () async {
+  test('project() should return projections for displayName', () async {
     Device deviceWithPopulatedQuery =
-        Device.findById("616e8382-bb26-41f5-8861-eeb62525d60")
+        Device.findById(testDeviceId)
           ..project([DeviceFields.displayName]);
     Device device = await deviceWithPopulatedQuery.get();
     expect(device.displayName, isA<String>());
     expect(() => device.id, throwsA('id is not projected'));
   });
 
-  // test('project() should return projections for lastActivityAt', () async {
-  //   User userWithPopulatedQuery = User.find()
-  //     ..project([UserFields.lastActivityAt]);
-  //   User user = await userWithPopulatedQuery.get();
-  //   expect(user.lastActivityAt, isA<DateTime>());
-  //   expect(() => user.id, throwsA('id is not projected'));
-  //   expect(() => user.firstActivityAt,
-  //       throwsA('firstActivityAt is not projected'));
+  test('project() should return projections for description', () async {
+    Device deviceWithPopulatedQuery =
+    Device.findById(testDeviceId)
+      ..project([DeviceFields.description]);
+    Device device = await deviceWithPopulatedQuery.get();
+    expect(device.description, isA<String>());
+    expect(() => device.id, throwsA('id is not projected'));
+  });
+
+  test('project() should return projections for manufacturerName', () async {
+    Device deviceWithPopulatedQuery =
+    Device.findById(testDeviceId)
+      ..project([DeviceFields.manufacturerName]);
+    Device device = await deviceWithPopulatedQuery.get();
+    expect(device.manufacturerName, isA<String>());
+    expect(() => device.id, throwsA('id is not projected'));
+  });
+
+  test('project() should return projections for model', () async {
+    Device deviceWithPopulatedQuery =
+    Device.findById(testDeviceId)
+      ..project([DeviceFields.model]);
+    Device device = await deviceWithPopulatedQuery.get();
+    expect(device.model, isA<String>());
+    expect(() => device.id, throwsA('id is not projected'));
+  });
+
+  test('project() should return projections for firmwareVersion', () async {
+    Device deviceWithPopulatedQuery =
+    Device.findById(testDeviceId)
+      ..project([DeviceFields.firmwareVersion]);
+    Device device = await deviceWithPopulatedQuery.get();
+    expect(device.firmwareVersion, isA<String>());
+    expect(() => device.id, throwsA('id is not projected'));
+  });
+
+  test('project() should return projections for softwareVersion', () async {
+    Device deviceWithPopulatedQuery =
+    Device.findById(testDeviceId)
+      ..project([DeviceFields.softwareVersion]);
+    Device device = await deviceWithPopulatedQuery.get();
+    expect(device.softwareVersion, isA<String>());
+    expect(() => device.id, throwsA('id is not projected'));
+  });
+
+  test('project() should return projections for serialNumber', () async {
+    Device deviceWithPopulatedQuery =
+    Device.findById(testDeviceId)
+      ..project([DeviceFields.serialNumber]);
+    Device device = await deviceWithPopulatedQuery.get();
+    expect(device.serialNumber, isA<String>());
+    expect(() => device.id, throwsA('id is not projected'));
+  });
+
   // });
 }
