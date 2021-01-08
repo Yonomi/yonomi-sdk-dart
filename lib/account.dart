a238d43a04b49dd4e51a26687182dd388dbe8d77
Sergio E <ses110@gmail.com>
2021-01-08 13:00:48 -0600

    fix: add missing variables key-value entry for integrationId to be passed into mutation

--- lib/account.dart
+++ lib/account.dart
@@ -6,26 +6,27 @@ import 'package:http/http.dart' as http;
 import 'config.dart';
 
 class account {
-
   static Future<String> generateAccountLinkingUrl(String integrationId) async {
-    var graphQlQuery = {
-      'query':
-          "mutation generateAccountLinkingUrl (\$integrationId: ID!) { generateAccountLinkingUrl(integrationId: \"${integrationId}\") { url }"
+    var graphQlMutation = {
+      'query': '''mutation generateAccountLinkingUrl (\$integrationId: ID!) { 
+                generateAccountLinkingUrl(integrationId: \$integrationId) { url } 
+            }
+          ''',
+      'variables': '{"integrationId": "${integrationId}"}'
     };
 
     String bearerToken = 'Bearer ${CONFIG.TOKEN}';
     String url = '${CONFIG.URL}';
     var response = await http.post(url,
-        body: jsonEncode(graphQlQuery),
+        body: jsonEncode(graphQlMutation),
         headers: {
           HttpHeaders.authorizationHeader: bearerToken,
           'Content-Type': 'application/json'
         });
 
-    Map<String, String> accountMap = jsonDecode(response.body)['data']
-        ['generateAccountLinkingUrl'] as Map<String, String>;
+    Map<String, dynamic> accountMap = jsonDecode(response.body)['data']
+        ['generateAccountLinkingUrl'] as Map<String, dynamic>;
 
-    return accountMap['url'];
+    return accountMap['url'] as String;
   }
-
 }
