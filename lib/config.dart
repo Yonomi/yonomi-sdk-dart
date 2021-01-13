7c1f70106fad9952b07d88f3e6c6ce9f738d0542
Sergio E <ses110@gmail.com>
2021-01-13 12:05:44 -0600

    feature: Use .env file to add your token

--- lib/config.dart
+++ lib/config.dart
@@ -1,21 +1,44 @@
 import 'dart:io';
 
+import 'package:dotenv/dotenv.dart' show load, env;
 import 'package:safe_config/safe_config.dart';
 
+/// Configuration:
+///
+/// 1.Create ".env" file (Do not check-in!)
+///
+///  Place your token in this format:
+///
+///  token="YOUR-TOKEN"
+///
+///
+///  2. Create or modify "config.yaml" file
+///  Place your graphQL endpoint in this format:
+///
+///  graphEndpoint: "YOUR-ENDPOINT"
+///
 class CONFIG extends Configuration {
+  static final configFilename = "config.yaml", tokenKey = "myToken";
+
   static final CONFIG _instance = CONFIG._privateConstructor();
 
   factory CONFIG() {
     return _instance;
   }
 
-  CONFIG._privateConstructor() : super.fromFile(File("config.yaml"));
+  CONFIG._privateConstructor() : super.fromFile(File(configFilename)) {
+    load(); // Load .env
+  }
+
+  String _getToken() {
+    return env[tokenKey];
+  }
 
   @optionalConfiguration
-  static final String TOKEN = _instance.authToken;
+  static final String TOKEN = _instance._getToken();
 
   @optionalConfiguration
   static final String URL = _instance.graphEndpoint;
 
-  String graphEndpoint, authToken;
+  String graphEndpoint;
 }
