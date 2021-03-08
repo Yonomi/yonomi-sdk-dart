ce7a46d3ad8d7a7cf8210d44bf64f59c8d8a6b36
Sergio E <ses110@gmail.com>
2021-03-08 13:06:05 -0600

    chore: move repository test files into respective folders

---
+++ test/e2e/repository/user_repository.dart
@@ -0,0 +1,7 @@
+import 'package:test/test.dart';
+
+void main() {
+  // yoRequest.Request request = yoRequest.Request(
+  //     CONFIG.URL, {HttpHeaders.authorizationHeader: 'Bearer ${CONFIG.TOKEN}'});
+  test('', () async {});
+}
