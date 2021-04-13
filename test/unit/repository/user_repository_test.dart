02aa9cb1649c6fc1c880cf0408261503f0a1aec7
Sergio E <ses110@gmail.com>
2021-04-13 11:40:32 -0600

    fixed mock in unit test for user_repository_test.dart

--- test/unit/repository/user_repository_test.dart
+++ test/unit/repository/user_repository_test.dart
@@ -1,16 +1,16 @@
 import 'package:artemis/artemis.dart';
 import 'package:mockito/mockito.dart';
 import 'package:test/test.dart';
-import 'package:yonomi_platform_sdk/graphql/user_query.dart';
+import 'package:yonomi_platform_sdk/graphql/user/user_query.dart';
 import 'package:yonomi_platform_sdk/repository/user_repository.dart';
 import 'package:yonomi_platform_sdk/request/request.dart';
 
 class MockClient extends Mock implements ArtemisClient {}
 
 void main() {
-  GraphQLResponse buildResponse(
+  GraphQLResponse<User$Query> buildResponse(
       String id, DateTime firstActivityAt, DateTime lastActivityAt) {
-    return GraphQLResponse(
+    return GraphQLResponse<User$Query>(
         data: User$Query.fromJson({
       "me": {
         "id": id,
@@ -32,8 +32,8 @@ void main() {
     DateTime firstActivityAt = DateTime.now();
     DateTime lastActivityAt = DateTime.now();
 
-    when(mockClient.execute(any)).thenAnswer((_) => Future.value(
-        buildResponse(expectedId, firstActivityAt, lastActivityAt)));
+    when(mockClient.execute(any)).thenAnswer((_) async =>
+        buildResponse(expectedId, firstActivityAt, lastActivityAt));
 
     User user = await UserRepository.getUserDetails(req, client: mockClient);
 
