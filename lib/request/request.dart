5c3ad672a11005706360e7cf57ef1d97a556afc7
Suraj Adhikari <suraj.adhikari@yonomi.co>
2021-04-16 07:33:40 -0500

    Created client from userId

--- lib/request/request.dart
+++ lib/request/request.dart
@@ -4,3 +4,12 @@ class Request {
 
   Request(this.graphUrl, this.headers);
 }
+
+class RequestParam {
+  final String graphUrl;
+  final String privateKey;
+  final String tenantId;
+  final String userId;
+
+  RequestParam(this.graphUrl, this.privateKey, this.tenantId, this.userId);
+}
