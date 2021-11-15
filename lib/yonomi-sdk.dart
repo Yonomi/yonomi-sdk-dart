40056d851b3f07b21785705e1ed66bedb9d4a603
Sergio E <ses110@gmail.com>
2021-11-15 14:09:44 -0600

    build(yonomi-sdk.dart): use exports to prevent library consumers from importing lib/src

---
+++ lib/yonomi-sdk.dart
@@ -0,0 +1,6 @@
+export 'src/repository/account_repository.dart';
+export 'src/repository/devices/devices_repository.dart';
+export 'src/repository/devices/lock_repository.dart';
+export 'src/repository/gql_client.dart';
+export 'src/repository/user_repository.dart';
+export 'src/request/request.dart';
