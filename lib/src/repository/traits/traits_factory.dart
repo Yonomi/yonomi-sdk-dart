4c1050225d62481b7e07a0596763124fff160534
Suraj Adhikari <suraj.adhikari@yonomi.co>
2021-12-02 10:12:24 -0600

    feat: adding queries for device detail page

---
+++ lib/src/repository/traits/traits_factory.dart
@@ -0,0 +1,14 @@
+import 'package:yonomi_platform_sdk/third_party/yonomi_graphql_schema/schema.docs.schema.gql.dart';
+import 'package:yonomi_platform_sdk/yonomi-sdk.dart';
+
+typedef TraitName = GTraitName;
+
+class TraitsFactory {
+  static Trait getTrait(TraitName name) {
+    switch (name) {
+      case TraitName.LOCK:
+        return new LockTrait();
+      default:
+        return new UnknownTrait();
+    }
+}
