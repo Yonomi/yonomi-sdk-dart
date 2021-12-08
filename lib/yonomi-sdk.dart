1fafc95e0be9ea875ba2979447d5da07b8c861c5
Suraj Adhikari <suraj.adhikari@yonomi.co>
2021-12-08 12:59:23 -0600

    fix: added export

--- lib/yonomi-sdk.dart
+++ lib/yonomi-sdk.dart
@@ -1,6 +1,6 @@
 export 'src/repository/account_repository.dart';
 export 'src/repository/devices/devices_repository.dart';
-export 'src/repository/devices/lock_repository.dart';
+export 'src/repository/traits/lock_repository.dart';
 export 'src/repository/devices/thermostat_repository.dart';
 export 'src/repository/gql_client.dart';
 export 'src/repository/traits/power_repository.dart';
