11d5e31d82f382140f26fd1b6f30234cc767af58
Sergio E <ses110@gmail.com>
2022-02-14 09:24:04 -0600

    refactor(devices_repository): extract parsing to LockRepository class

--- lib/src/repository/devices/devices_repository.dart
+++ lib/src/repository/devices/devices_repository.dart
@@ -1,4 +1,3 @@
-import 'package:gql_exec/gql_exec.dart' as gql;
 import 'package:gql_link/gql_link.dart';
 import 'package:yonomi_platform_sdk/src/queries/devices/get_device/query.data.gql.dart';
 import 'package:yonomi_platform_sdk/src/queries/devices/get_device/query.req.gql.dart';
@@ -6,6 +5,7 @@ import 'package:yonomi_platform_sdk/src/queries/devices/get_devices/query.data.g
 import 'package:yonomi_platform_sdk/src/queries/devices/get_devices/query.req.gql.dart';
 import 'package:yonomi_platform_sdk/src/repository/base_repository.dart';
 import 'package:yonomi_platform_sdk/src/repository/gql_client.dart';
+import 'package:yonomi_platform_sdk/src/repository/traits/lock_repository.dart';
 import 'package:yonomi_platform_sdk/src/repository/traits/thermostat_repository.dart';
 import 'package:yonomi_platform_sdk/src/request/request.dart';
 import 'package:yonomi_platform_sdk/third_party/yonomi_graphql_schema/schema.docs.schema.gql.dart';
@@ -101,7 +101,7 @@ class DevicesRepository {
         case GTraitName.THERMOSTAT_SETTING:
           return ThermostatRepository.getThermostatTrait(trait);
         case GTraitName.LOCK:
-          return getLockTrait(trait);
+          return LockRepository.getLockTrait(trait);
         case GTraitName.BATTERY_LEVEL:
           return getBatteryLevelTrait(trait);
         case GTraitName.POWER:
@@ -110,18 +110,6 @@ class DevicesRepository {
           return UnknownTrait(trait.name.toString());
       }
     }).toList();
-  }
-
-  static LockTrait getLockTrait(dynamic trait) {
-    if (trait is GgetDeviceData_device_traits__asLockDeviceTrait ||
-        trait
-            is GgetDevicesData_me_devices_edges_node_traits__asLockDeviceTrait) {
-      return LockTrait(IsLocked(trait.state.isLocked.reported?.value ?? false),
-          supportsIsJammed:
-              SupportsIsJammed(trait.properties.supportsIsJammed ?? false));
-    } else {
-      throw ArgumentError.value(trait);
-    }
   }
 
   static PowerTrait getPowerTrait(dynamic trait) {
--- lib/src/repository/traits/lock_repository.dart
+++ lib/src/repository/traits/lock_repository.dart
@@ -1,8 +1,22 @@
+import 'package:yonomi_platform_sdk/src/queries/devices/get_device/query.data.gql.dart';
+import 'package:yonomi_platform_sdk/src/queries/devices/get_devices/query.data.gql.dart';
 import 'package:yonomi_platform_sdk/src/queries/lock/make_lock_unlock_action_request/query.req.gql.dart';
 import 'package:yonomi_platform_sdk/src/repository/base_repository.dart';
 import 'package:yonomi_platform_sdk/yonomi-sdk.dart';
 
 class LockRepository {
+  static LockTrait getLockTrait(trait) {
+    if (trait is GgetDeviceData_device_traits__asLockDeviceTrait ||
+        trait
+            is GgetDevicesData_me_devices_edges_node_traits__asLockDeviceTrait) {
+      return LockTrait(IsLocked(trait.state.isLocked.reported?.value ?? false),
+          supportsIsJammed:
+              SupportsIsJammed(trait.properties.supportsIsJammed ?? false));
+    } else {
+      throw ArgumentError.value(trait);
+    }
+  }
+
   static Future<void> sendLockUnlockAction(
       Request request, String id, bool lockUnlock) async {
     final link = GraphLinkCreator.create(request);
