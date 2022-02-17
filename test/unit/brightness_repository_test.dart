4e0728b7942fe97b86beaec1a5615df3474caf30
Rigoberto L. Perez <rigoberto.perez@allegion.com>
2022-02-17 15:55:48 -0500

    test: Add tests for brightness trait

---
+++ test/unit/brightness_repository_test.dart
@@ -0,0 +1,27 @@
+import 'package:gql_exec/gql_exec.dart';
+import 'package:gql_link/gql_link.dart';
+import 'package:mockito/annotations.dart';
+import 'package:mockito/mockito.dart';
+import 'package:test/test.dart';
+import 'package:yonomi_platform_sdk/src/repository/repository.dart';
+import 'package:yonomi_platform_sdk/src/repository/traits/brightness_repository.dart';
+import 'package:yonomi_platform_sdk/yonomi-sdk.dart' as sdk;
+
+import 'brightness_repository_test.mocks.dart';
+
+@GenerateMocks([
+  Link,
+  Response
+], customMocks: [
+  MockSpec<sdk.Request>(as: #MockRequest, returnNullOnMissingStub: true)
+])
+void main() {
+  test('BrightnessRepository calls client request with passed id', () async {
+    final request = MockRequest();
+    when(request.headers).thenReturn(Map<String, String>());
+    when(request.graphUrl).thenReturn('https://platform.yonomi.cloud/graphql');
+    await BrightnessRepository.setBrightnessAction(request, 'id', 75);
+    verify(request.graphUrl).called(1);
+    verify(request.headers).called(1);
+  });
+}
