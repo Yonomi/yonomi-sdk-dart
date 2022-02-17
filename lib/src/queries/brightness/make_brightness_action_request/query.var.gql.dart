9dfbeed0083020b2e0af5c59f837e2335985e052
Rigoberto L. Perez <rigoberto.perez@allegion.com>
2022-02-17 15:03:49 -0500

    feat: Add brightness actions query

---
+++ lib/src/queries/brightness/make_brightness_action_request/query.var.gql.dart
@@ -0,0 +1,31 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+import 'package:built_value/built_value.dart';
+import 'package:built_value/serializer.dart';
+import 'package:yonomi_platform_sdk/third_party/yonomi_graphql_schema/serializers.gql.dart'
+    as _i1;
+
+part 'query.var.gql.g.dart';
+
+abstract class GmakeBrightnessActionRequestVars
+    implements
+        Built<GmakeBrightnessActionRequestVars,
+            GmakeBrightnessActionRequestVarsBuilder> {
+  GmakeBrightnessActionRequestVars._();
+
+  factory GmakeBrightnessActionRequestVars(
+          [Function(GmakeBrightnessActionRequestVarsBuilder b) updates]) =
+      _$GmakeBrightnessActionRequestVars;
+
+  String get deviceId;
+  int get brightness;
+  static Serializer<GmakeBrightnessActionRequestVars> get serializer =>
+      _$gmakeBrightnessActionRequestVarsSerializer;
+  Map<String, dynamic> toJson() => (_i1.serializers
+          .serializeWith(GmakeBrightnessActionRequestVars.serializer, this)
+      as Map<String, dynamic>);
+  static GmakeBrightnessActionRequestVars? fromJson(
+          Map<String, dynamic> json) =>
+      _i1.serializers
+          .deserializeWith(GmakeBrightnessActionRequestVars.serializer, json);
+}
