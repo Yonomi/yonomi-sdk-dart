c10c8e4d210c5477ebe6a2c4f921738cfa6c0b24
Rigoberto L. Perez <rigoberto.perez@allegion.com>
2022-02-24 13:55:40 -0500

    fix(power_repository.dart): Don't use defaults for values that come back null

--- lib/src/repository/traits/power_repository.dart
+++ lib/src/repository/traits/power_repository.dart
@@ -1,20 +1,16 @@
 import 'package:gql_link/gql_link.dart';
-import 'package:yonomi_platform_sdk/src/queries/devices/get_device/query.data.gql.dart';
-import 'package:yonomi_platform_sdk/src/queries/devices/get_devices/query.data.gql.dart';
 import 'package:yonomi_platform_sdk/src/queries/power/make_power_action_request/query.req.gql.dart';
 import 'package:yonomi_platform_sdk/src/repository/repository.dart';
 import 'package:yonomi_platform_sdk/yonomi-sdk.dart';
 
 class PowerRepository {
   static PowerTrait getPowerTrait(dynamic trait) {
-    if (trait is GgetDeviceData_device_traits__asPowerDeviceTrait ||
-        trait
-            is GgetDevicesData_me_devices_edges_node_traits__asPowerDeviceTrait) {
-      return PowerTrait(IsOnOff(trait.state.isOn.reported?.value ?? false),
-          supportsDiscreteOnOff: SupportsDiscreteOnOff(
-              trait.properties.supportsDiscreteOnOff ?? false));
-    } else {
-      throw ArgumentError.value(trait);
+    try {
+      return PowerTrait(IsOnOff(trait.state.isOn.reported?.value),
+          supportsDiscreteOnOff:
+              SupportsDiscreteOnOff(trait.properties.supportsDiscreteOnOff));
+    } on NoSuchMethodError {
+      throw ArgumentError.value(trait, 'PowerTrait', 'Invalid PowerTrait');
     }
   }
 
@@ -33,12 +29,12 @@ class PowerRepository {
   }
 }
 
-class SupportsDiscreteOnOff extends Property<bool> {
-  SupportsDiscreteOnOff(bool value) : super('supportsDiscreteOnOff', value);
+class SupportsDiscreteOnOff extends Property<bool?> {
+  SupportsDiscreteOnOff(bool? value) : super('supportsDiscreteOnOff', value);
 }
 
-class IsOnOff extends State<bool> {
-  IsOnOff(bool value) : super('isOn', value);
+class IsOnOff extends State<bool?> {
+  IsOnOff(bool? value) : super('isOn', value);
 }
 
 class PowerTrait extends Trait {
