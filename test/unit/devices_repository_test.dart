136f066b2206f2b60359e3f63cc238ad39af7ba4
Sergio E <ses110@gmail.com>
2022-04-19 09:50:36 -0600

    test(colortemperaturerepository): test to cover ArgumentError case

--- test/unit/devices_repository_test.dart
+++ test/unit/devices_repository_test.dart
@@ -476,6 +476,13 @@ void main() {
   });
 
   test(
+      'getColorTemperatureTrait should throw argumentError if trait object is not correct type',
+      () {
+    expect(() => ColorTemperatureRepository.getColorTemperatureTrait(null),
+        throwsA(isA<ArgumentError>()));
+  });
+
+  test(
       'getBatteryLevelTrait should throw argumentError if trait object is not correct type',
       () {
     expect(() => BatteryLevelRepository.getBatteryLevelTrait(null),
