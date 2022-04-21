2cd2be2e81542dac3181ddac3eb794ad98b72c0d
Rigoberto L. Perez <rigoberto.perez@allegion.com>
2022-04-21 01:22:33 -0400

    fix(devices_repository.dart): Remove code that is not relevant to device repo
    BREAKING CHANGE:

--- lib/src/repository/devices_repository.dart
+++ lib/src/repository/devices_repository.dart
@@ -122,17 +122,6 @@ class DevicesRepository {
       }
     }).toList();
   }
-
-  static BatteryLevelTrait getBatteryLevelTrait(dynamic trait) {
-    if (trait is GgetDeviceData_device_traits__asBatteryLevelDeviceTrait ||
-        trait
-            is GgetDevicesData_me_devices_edges_node_traits__asBatteryLevelDeviceTrait) {
-      return BatteryLevelTrait(
-          BatteryLevel(trait.state.percentage.reported?.value ?? 0));
-    } else {
-      throw ArgumentError.value(trait);
-    }
-  }
 }
 
 class Device {
