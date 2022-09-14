d959097c688d9f8751db2dc8fcf1a5461be9aa96
Sergio E <ses110@gmail.com>
2022-09-14 13:27:11 -0600

    test(pin_code_repository.dart): add test for secondary PinCodeCredentials constructor

--- test/unit/pin_code_repository_test.dart
+++ test/unit/pin_code_repository_test.dart
@@ -1,6 +1,7 @@
 import 'package:mockito/mockito.dart';
 import 'package:test/test.dart';
 import 'package:yonomi_platform_sdk/src/repository/traits/pin_code_repository.dart';
+import 'package:yonomi_platform_sdk/yonomi-sdk.dart';
 
 import 'base_mock_test.dart';
 
@@ -17,5 +18,21 @@ main() {
       () {
     expect(() => PinCodeRepository.getPinCodeTrait(null),
         throwsA(isA<ArgumentError>()));
+  });
+  test(
+      'PinCodeCredentials.withCredentials should correctly build a list of credentials in a PinCodeTrait',
+      () {
+    var pinCodeTrait = PinCodeTrait(<State>{
+      PinCodeCredentials.withCredentials(
+        [
+          PinCodeCredential('Admin', '5678'),
+          PinCodeCredential('Plant Lady', '1234')
+        ],
+      ),
+    }, {});
+    expect(pinCodeTrait.pinCodes![0].name, 'Admin');
+    expect(pinCodeTrait.pinCodes![0].pinCode, '5678');
+    expect(pinCodeTrait.pinCodes![1].name, 'Plant Lady');
+    expect(pinCodeTrait.pinCodes![1].pinCode, '1234');
   });
 }
